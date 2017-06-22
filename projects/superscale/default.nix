07acfda62867954dd964e6518236a00ed1d14c43
Bastian Köcher <bastian@superscale.io>
2017-06-22 16:37:59 +0200

    Adds go dependencies

--- projects/superscale/default.nix
+++ projects/superscale/default.nix
@@ -4,8 +4,8 @@ let
 in
 stdenv.mkDerivation {
   name = "superscale";
-  buildInputs = [ myrust elixir go erlang openssl llvm minicom json_c qemu iw pkgconfig elixir qemu socat clang ];
+  buildInputs = [ myrust elixir go erlang openssl llvm minicom json_c qemu iw pkgconfig elixir qemu socat clang godef gocode go ];
   RUST_SRC_PATH="${myrust}/lib/rustlib/src/rust/src";
-  GOPATH="$PWD/gopath";
+  GOPATH="$PWD/goprojects";
   LIBCLANG_PATH="${llvmPackages.clang-unwrapped}/lib";
 }
