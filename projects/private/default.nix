8d9f4fe6a2c62f353f4e9ccec4b3b850912b83ea
Bastian Köcher <git@kchr.de>
2017-07-12 20:14:05 +0200

    Add rust for private and docker_compose for superscale

--- projects/private/default.nix
+++ projects/private/default.nix
@@ -1,5 +1,9 @@
 with import <nixpkgs> {};
+let
+  myrust = (rustChannels.nightly.rust.override { extensions = [ "rust-src" ];});
+in
 stdenv.mkDerivation {
   name = "private";
-  buildInputs = [ elmPackages.elm nodejs texlive.combined.scheme-full ];
+  buildInputs = [ myrust elmPackages.elm nodejs texlive.combined.scheme-full ];
+  RUST_SRC_PATH="${myrust}/lib/rustlib/src/rust/src";
 }
--- projects/superscale/default.nix
+++ projects/superscale/default.nix
@@ -4,7 +4,7 @@ let
 in
 stdenv.mkDerivation {
   name = "superscale";
-  buildInputs = [ myrust elixir go erlang openssl llvm minicom json_c qemu iw pkgconfig elixir qemu socat clang godef gocode go bluez ];
+  buildInputs = [ myrust elixir go erlang openssl llvm minicom json_c qemu iw pkgconfig elixir qemu socat clang godef gocode go bluez docker_compose ];
   RUST_SRC_PATH="${myrust}/lib/rustlib/src/rust/src";
   GOPATH="$PWD/goprojects";
   LIBCLANG_PATH="${llvmPackages.clang-unwrapped}/lib";
