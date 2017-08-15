020472ce0e7a07f416620d6a8207cad3ab0ae7e1
Bastian Köcher <git@kchr.de>
2017-08-15 09:33:58 +0200

    Increase the font again

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -133,7 +133,7 @@ values."
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 12
+                               :size 18
                                :weight normal
                                :width normal
                                :powerline-scale 1.3)
