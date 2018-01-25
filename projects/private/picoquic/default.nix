45155ec3d0d03db596ca88648a7da729502541aa
Bastian Köcher <git@kchr.de>
2018-01-25 09:07:06 +0100

    Dependencies update

--- projects/private/picoquic/default.nix
+++ projects/private/picoquic/default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "picoquic";
   buildInputs = [ openssl pkgconfig clang cmake ];
-  LIBCLANG_PATH="${llvmPackages.clang-unwrapped}/lib";
+  LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
   '';
--- projects/private/rust/default.nix
+++ projects/private/rust/default.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "rust";
-  buildInputs = [ python2 cmake nodejs-9_x wabt ];
+  buildInputs = [ python2 cmake nodejs-9_x wabt openssl pkgconfig zlib ];
 }
