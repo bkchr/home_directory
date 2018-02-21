1fd28de270dfce30b23134c0c1c89525989dac9f
Bastian Köcher <git@kchr.de>
2018-02-21 20:48:10 +0100

    Adds carrier folder stuff

---
+++ projects/private/carrier/default.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "picoquic";
+  buildInputs = [ openssl pkgconfig clang cmake ];
+  LIBCLANG_PATH="${llvmPackages.libclang}/lib";
+  shellHook = ''
+    export NIX_CXXSTDLIB_LINK=""
+  '';
+} 
