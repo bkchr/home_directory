fd3bbcae9a2e0e4ed2a44f15979bf6e190de214b
Bastian Köcher <git@kchr.de>
2018-02-27 15:29:20 +0100

    Some updates for the superscale folder

--- projects/superscale/default.nix
+++ projects/superscale/default.nix
@@ -1,10 +1,14 @@
 with import <nixpkgs> {};
 let
-  myrust = (latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rust-analysis" ]; targets = [ "mips-unknown-linux-musl" "mipsel-unknown-linux-musl" ]; });
+  myrust = (latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ]; targets = [ "mips-unknown-linux-musl" "mipsel-unknown-linux-musl" ];});
 in
 stdenv.mkDerivation {
   name = "superscale";
   buildInputs = [ myrust elixir go openssl llvm minicom json_c qemu iw pkgconfig qemu clang socat godef gocode bluez docker_compose libseccomp nomad terraform goimports consul quilt cmake ];
   RUST_SRC_PATH="${myrust}/lib/rustlib/src/rust/src";
-  LIBCLANG_PATH="${llvmPackages.clang-unwrapped}/lib";
+  LIBCLANG_PATH="${llvmPackages.libclang}/lib";
+  shellHook = ''
+    export NIX_CXXSTDLIB_LINK=""
+  '';
+
 } 
