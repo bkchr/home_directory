6265c5dab0f9b029c246ed72f2e5e56f6432dd55
Bastian Köcher <git@kchr.de>
2018-03-30 20:02:29 +0200

    Adds python stuff for hole punch

--- projects/private/hole_punch/default.nix
+++ projects/private/hole_punch/default.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "hole_punch";
-  buildInputs = [ openssl pkgconfig clang ];
+  buildInputs = [ openssl pkgconfig clang python2 python2Packages.yapf ];
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
