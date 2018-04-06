0b85bff747bc0260be7ee20fb5c3199cef1ccb7e
Bastian Köcher <git@kchr.de>
2018-04-06 14:48:39 +0200

    Adds all required dependencies for gstreamer-rs

--- projects/private/gstreamer-rs/default.nix
+++ projects/private/gstreamer-rs/default.nix
@@ -1,5 +1,6 @@
 with import <nixpkgs> {};
+
 stdenv.mkDerivation {
   name = "gstreamer-rs";
-  buildInputs = [ gst_all_1.gstreamer pkgconfig gst_all_1.gst-plugins-base gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-good gst_all_1.gst-rtsp-server gst_all_1.gst-plugins-ugly ];
+  buildInputs = [ gst_all_1.gstreamer pkgconfig gst_all_1.gst-plugins-base gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-good gst_all_1.gst-plugins-ugly gst_all_1.gst-rtsp-server ];
 }
