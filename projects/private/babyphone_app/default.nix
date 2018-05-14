d4a9ede0dbc34162c7c4999d793fc3f4e7386d9b
Bastian Köcher <git@kchr.de>
2018-05-14 10:23:43 +0200

    New direnv nix_shell implementation

--- projects/private/babyphone_app/default.nix
+++ projects/private/babyphone_app/default.nix
@@ -1,11 +1,8 @@
 with import <nixpkgs> {};
-let 
-  myopencv = opencv3.override { enableGtk3 = true; };
-in
 stdenv.mkDerivation {
   name = "private";
-  buildInputs = [ openssl pkgconfig clang cmake myopencv libv4l ];
-  ANDROID_NDK_HOME = "${androidenv.androidndk}";
+  buildInputs = [ openssl pkgconfig clang cmake ];
+  ANDROID_NDK_HOME = "${androidenv.androidndk}/libexec/android-ndk-r10e";
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
