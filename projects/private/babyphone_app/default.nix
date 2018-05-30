9e04059729d42cb6f7792a04a674b505f2cf249c
Bastian Köcher <git@kchr.de>
2018-05-30 14:21:47 +0200

    Small changes

--- projects/private/babyphone_app/default.nix
+++ projects/private/babyphone_app/default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "private";
   buildInputs = [ openssl pkgconfig clang cmake ];
-  ANDROID_NDK_HOME = "${androidenv.androidndk}/libexec/${androidenv.androidndk.name}";
+  ANDROID_NDK_HOME = "${androidenv.androidndk_16b}/libexec/${androidenv.androidndk_16b.name}";
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
