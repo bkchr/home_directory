453f2f3591fe1057a1aa0dcd295a46e36fdda97e
Bastian Köcher <git@kchr.de>
2018-07-10 16:32:35 +0200

    Stuff

--- projects/private/babyphone_app/default.nix
+++ projects/private/babyphone_app/default.nix
@@ -1,8 +1,11 @@
 with import <nixpkgs> {};
+let
+  androidndk = androidenv.androidndk_16b.override { fullNDK = true; };
+in
 stdenv.mkDerivation {
   name = "private";
-  buildInputs = [ openssl pkgconfig clang cmake ];
-  ANDROID_NDK_HOME = "${androidenv.androidndk_16b}/libexec/${androidenv.androidndk_16b.name}";
+  buildInputs = [ openssl pkgconfig clang cmake gst_all_1.gstreamer ];
+  ANDROID_NDK_HOME = "${androidndk}/libexec/${androidndk.name}";
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
--- projects/private/gstreamer-rs/default.nix
+++ projects/private/gstreamer-rs/default.nix
@@ -1,7 +1,10 @@
 with import <nixpkgs> {};
 
+let
+  bad = gst_all_1.gst-plugins-bad.override { faacSupport = true; };
+in
 stdenv.mkDerivation {
   name = "gstreamer-rs";
-  buildInputs = [ gst_all_1.gstreamer pkgconfig gst_all_1.gst-plugins-base gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-good gst_all_1.gst-plugins-ugly gst_all_1.gst-rtsp-server gst_all_1.gst-libav ];
+  buildInputs = [ gst_all_1.gstreamer pkgconfig gst_all_1.gst-plugins-base bad gst_all_1.gst-plugins-good gst_all_1.gst-plugins-ugly gst_all_1.gst-rtsp-server gst_all_1.gst-libav ];
   ANDROID_NDK_HOME = "${androidenv.androidndk}/libexec/${androidenv.androidndk.name}";
 }
