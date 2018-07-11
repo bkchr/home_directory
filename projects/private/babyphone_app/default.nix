faf14384f9feb72257b89c6ed016e170c86a2753
Bastian Köcher <git@kchr.de>
2018-07-11 13:15:16 +0200

    Update dependencies

--- projects/private/babyphone_app/default.nix
+++ projects/private/babyphone_app/default.nix
@@ -1,10 +1,11 @@
 with import <nixpkgs> {};
 let
   androidndk = androidenv.androidndk_16b.override { fullNDK = true; };
+  bad = gst_all_1.gst-plugins-bad.override { faacSupport = true; };
 in
 stdenv.mkDerivation {
   name = "private";
-  buildInputs = [ openssl pkgconfig clang cmake gst_all_1.gstreamer ];
+  buildInputs = [ openssl pkgconfig clang cmake bad gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good ];
   ANDROID_NDK_HOME = "${androidndk}/libexec/${androidndk.name}";
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
