92499c0bbd23d1c141dc79385f7249e34464f6a2
Bastian Köcher <git@kchr.de>
2018-08-14 16:16:40 +0200

    Do not add ANDROID_NDK on none x86_64 systems

--- projects/private/babyphone_app/default.nix
+++ projects/private/babyphone_app/default.nix
@@ -7,7 +7,7 @@ in
 stdenv.mkDerivation {
   name = "private";
   buildInputs = [ myrust openssl pkgconfig clang cmake bad gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good dbus ];
-  ANDROID_NDK_HOME = "${androidndk}/libexec/${androidndk.name}";
+  ANDROID_NDK_HOME = if stdenv.isx86_64 then "${androidndk}/libexec/${androidndk.name}" else "NOTSUPPORTED";
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
