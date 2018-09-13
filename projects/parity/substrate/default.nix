ef20c28aed5e2f902a25c43987dc7e79680dfbf3
Bastian Köcher <git@kchr.de>
2018-09-13 11:47:28 +0200

    Updates to nix files

--- projects/parity/substrate/default.nix
+++ projects/parity/substrate/default.nix
@@ -1,8 +1,20 @@
 with import <nixpkgs> {};
 let
   myrust = (latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" "clippy-preview" ]; targets = [ "wasm32-unknown-unknown" ]; });
+  wasm-gc = rustPlatform.buildRustPackage rec {
+    name = "wasm-gc-${version}";
+    version = "0.1.11";
+    src = fetchFromGitHub {
+      owner = "alexcrichton";
+      repo = "wasm-gc";
+      rev = "wasm-gc-api-${version}";
+      sha256 = "0nw6yhgn3d38s1dmh4jkiccsrks5i1cpjgb7r52l2n8cnry4h1zs";
+    };
+
+    cargoSha256 = "10ici7qfb1gxfc08ag6j7dn3b91c0irqr32pyrc6nz9q4aywc3b2";
+  };
 in
 stdenv.mkDerivation {
   name = "substrate";
-  buildInputs = [ myrust openssl pkgconfig cmake python3 ];
+  buildInputs = [ myrust wasm-gc openssl pkgconfig cmake python3 ];
 }
--- projects/private/hole_punch/default.nix
+++ projects/private/hole_punch/default.nix
@@ -1,7 +1,11 @@
 with import <nixpkgs> {};
+
+let
+  myrust = (latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" "clippy-preview" ];});
+in
 stdenv.mkDerivation {
   name = "hole_punch";
-  buildInputs = [ openssl pkgconfig clang python2 python2Packages.yapf qemu ];
+  buildInputs = [ openssl pkgconfig clang python2 python2Packages.yapf qemu myrust ];
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
