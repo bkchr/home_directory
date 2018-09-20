c2b37e536612931229bf6607218fdf6dfc2de0c1
Bastian Köcher <git@kchr.de>
2018-09-20 20:34:32 +0200

    Update babyphone app icon

--- projects/private/picoquic/default.nix
+++ projects/private/picoquic/default.nix
@@ -1,22 +1,8 @@
 with import <nixpkgs> {};
-let
-  openssl_1_1_1 = openssl_1_1_0.overrideAttrs (old: rec {
-    name = "openssl-${version}";
-    version = "1.1.1-pre8";
-    src = fetchurl {
-      url = "http://www.openssl.org/source/${name}.tar.gz";
-      sha256 = "07ij96wl5wj8v7mnv7fqhy2rwnbl0xdqlrchql692b6r7mvcs18j";
-    };
-    preConfigure = old.preConfigure + ''
-      substituteInPlace Configurations/unix-checker.pm --replace /usr/bin/perl ${perl}/bin/perl
-      patchShebangs .
-      substituteInPlace config --replace /usr/bin/env ""
-    '';
-  });
-in
+
 stdenv.mkDerivation {
   name = "picoquic";
-  buildInputs = [ openssl_1_1_1 pkgconfig clang cmake ];
+  buildInputs = [ openssl pkgconfig clang cmake ];
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
