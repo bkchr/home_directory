e312c68c11d6dbed47af766077f5ad282e586e55
Bastian Köcher <git@kchr.de>
2018-10-23 10:49:51 +0200

    Updates

--- projects/private/picoquic-c/default.nix
+++ projects/private/picoquic-c/default.nix
@@ -1,22 +1,7 @@
 with import <nixpkgs> {};
-let
-  openssl_1_1_1 = openssl_1_1_0.overrideAttrs (old: rec {
-    name = "openssl-${version}";
-    version = "1.1.1-pre8";
-    src = fetchurl {
-      url = "http://www.openssl.org/source/${name}.tar.gz";
-      sha256 = "07ij96wl5wj8v7mnv7fqhy2rwnbl0xdqlrchql692b6r7mvcs18j";
-    };
-    preConfigure = old.preConfigure + ''
-      substituteInPlace Configurations/unix-checker.pm --replace /usr/bin/perl ${perl}/bin/perl
-      patchShebangs .
-      substituteInPlace config --replace /usr/bin/env ""
-    '';
-  });
-in
 stdenv.mkDerivation {
   name = "picoquic";
-  buildInputs = [ openssl_1_1_1 pkgconfig cmake valgrind ];
+  buildInputs = [ openssl pkgconfig cmake valgrind ];
   LIBCLANG_PATH="${llvmPackages.clang-unwrapped}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
--- projects/private/picoquic/default.nix
+++ projects/private/picoquic/default.nix
@@ -1,8 +1,10 @@
 with import <nixpkgs> {};
-
+let
+  myrust = (latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" "clippy-preview" ];});
+in 
 stdenv.mkDerivation {
   name = "picoquic";
-  buildInputs = [ openssl pkgconfig clang cmake ];
+  buildInputs = [ myrust openssl pkgconfig clang cmake gdb ];
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
   shellHook = ''
     export NIX_CXXSTDLIB_LINK=""
